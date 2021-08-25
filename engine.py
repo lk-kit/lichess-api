d01162e0ac4d752f8aeac8ab4ad21385683b4c73
kieferro <nevour.spam@web.de>
2021-08-25 15:15:10 +0200

    "Get worst moves"-method

--- engine.py
+++ engine.py
@@ -1,10 +1,23 @@
 from stockfish import Stockfish
-import os.path
 
 
 class Engine(Stockfish):
-    def __init__(self, path):
+    def __init__(self, path, depth=10):
         Stockfish.__init__(self, path)
+
+        self.set_depth(depth)
+
+    def get_worst_moves(self):
+        top_moves = self.get_top_moves(141)
+
+        if len(top_moves) == 0:
+            raise AttributeError("Invalid position")
+
+        elif len(top_moves) == 1:
+            return {"status": -1}
+
+        else:
+            return {"status": 1, "worst": top_moves[-1], "second-worst": top_moves[-2]}
 
 
 if __name__ == "__main__":
@@ -12,6 +25,8 @@ if __name__ == "__main__":
 
     while True:
         try:
+            command = input(">>> ")
+
             exec(input(">>> "))
 
         except Exception as e:
