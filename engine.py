fa77ed88b36f6731bab16c4b7da0857c5920fd6c
kieferro <nevour.spam@web.de>
2021-08-25 14:57:08 +0200

    Renaming + class structure

---
+++ engine.py
@@ -0,0 +1,18 @@
+from stockfish import Stockfish
+import os.path
+
+
+class Engine(Stockfish):
+    def __init__(self, path):
+        Stockfish.__init__(self, path)
+
+
+if __name__ == "__main__":
+    engine = Engine("Stockfish\\Stockfish_12.exe")
+
+    while True:
+        try:
+            exec(input(">>> "))
+
+        except Exception as e:
+            print(f"Es ist ein Fehler aufgetreten: {e}")
--- stockfish_communication.py
+++
@@ -1,1 +0,0 @@
-from stockfish import Stockfish
